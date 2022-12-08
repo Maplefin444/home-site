a1339e7e649b67769867418d67383604415fa75f
Maplefin <james.huynh444@gmail.com>
2022-12-07 20:10:05 -0500

    autorefresh token

--- client/home-site/src/useAuth.js
+++ client/home-site/src/useAuth.js
@@ -11,10 +11,21 @@ export default function useAuth(code) {
             code,
         }).then(res => {
             setAccessToken(res.data.accessToken);
-            setRefreshToken(res.data.accessToken);
-            setExpiresIn(res.data.accessToken);
-            window.history.pushState({},null,'/');
+            setRefreshToken(res.data.refreshToken);
+            setExpiresIn(res.data.expiresIn);
+            window.history.pushState({}, null, '/');
         }).catch(() => window.location = '/');
     }, [code]);
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return;
+        const interval = setInterval(() => {
+            axios.post('http://localhost:3001/refresh', {
+                refreshToken,
+            }).then(res => {
+                setAccessToken(res.data.accessToken)
+                setExpiresIn(res.data.expiresIn)
+            }).catch(() => window.location = '/');
+        }, (expiresIn - 60) * 1000);
+    }, [refreshToken, expiresIn]);
     return accessToken;
 }
--- server/server.js
+++ server/server.js
@@ -6,6 +6,24 @@ const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: process.env.REDIRECT_URI,
+        refreshToken
+    });
+    spotifyApi.refreshAccessToken().then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in,
+        })
+    }).catch(err => {
+        res.sendStatus(400)
+    });
+});
+
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
