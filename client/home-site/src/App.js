b28288e1be851421aee232ec4e8d3047b43f08b5
Maplefin <james.huynh444@gmail.com>
2022-12-13 18:37:27 -0500

    cooler routing

--- client/home-site/src/App.js
+++ client/home-site/src/App.js
@@ -1,26 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MusicHome from './MusicHome'
 import Home from './Home.js'
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Navbar from './reusable_components/Navbar.js'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <ul className="App-header">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/musichome">Login</Link>
-          </li>
-        </ul>
+    <>
+      <Navbar />
+      <div className="container">
         <Routes>
-          <Route exact path='/' element={< Home/>}></Route>
-          <Route exact path='/musichome' element={< MusicHome/>}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/musichome" element={<MusicHome />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 }
 
