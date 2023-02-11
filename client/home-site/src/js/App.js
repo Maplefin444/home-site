ce2d559e9d9158b77667e16e2fc87e4dc5d82ea6
Maplefin <james.huynh444@gmail.com>
2023-02-10 22:39:06 -0500

    complete idea rework

--- client/home-site/src/js/App.js
+++ client/home-site/src/js/App.js
@@ -1,23 +1,56 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import MusicHome from './music/MusicHome'
-import Home from './Home.js'
-import RSHome from './runescape/RSHome'
-import Navbar from './reusable_components/Navbar.js'
-import { Routes, Route } from 'react-router-dom';
 import './app.css';
+import thock from './../images/thock.png';
+import rose from './../images/rose.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun, faEnvelope, faPlay, faMusic, faCalendar } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/musichome" element={<MusicHome />} />
-          <Route path="/rshome" element={<RSHome />}/>
-        </Routes>
+    <div className='container'>
+      <div className='info'>
+        <div className='time'>
+          24:00 AM
+        </div>
+
+        <div className='icon'>
+          <FontAwesomeIcon icon={faSun} size='2xl' />
+        </div>
+
       </div>
-    </>
+
+
+
+      <div className='buttons'>
+        <a className='button'>
+          <FontAwesomeIcon icon={faEnvelope} size='l' />
+        </a>
+        <a className='button'>
+          <FontAwesomeIcon icon={faPlay} size='l' />
+        </a>
+        <a className='button'>
+          <FontAwesomeIcon icon={faMusic} size='l' />
+        </a>
+        <a className='button'>
+          <FontAwesomeIcon icon={faCalendar} size='l' />
+        </a>
+        <a className='button'>
+          <FontAwesomeIcon icon={faEnvelope} size='l' />
+        </a>
+        <a className='button'>
+          <FontAwesomeIcon icon={faEnvelope} size='l' />
+        </a>
+      </div>
+
+      <div className='graphicOne'>
+        <img src={thock}></img>
+      </div>
+
+      <div className='line'></div>
+
+      <div className='graphicTwo'>
+        <img src={rose}></img>
+      </div>
+    </div>
   );
 }
 
